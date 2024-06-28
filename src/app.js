19f26845718e588668e8cb248d5e04c6dc6b5e11
--global <--global>
2024-06-28 23:55:55 +0530

    Scripts are added to app

--- src/app.js
+++ src/app.js
@@ -9,71 +9,18 @@ const winston = require("winston");
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const { expressCspHeader, SELF } = require("express-csp-header");
+const { expressCspHeader } = require("express-csp-header");
+const {
+  isDevelopment,
+  cspOptions,
+  corsOptions,
+  sessionConfig,
 
-const { MONGO_URL } = require("@Services/mongo");
+  logger,
+  accessLogStream,
+} = require("@Config/helperVariables");
 
 const app = express();
-
-/******** Defined variables and configs ********/
-// Define the log file path
-const logFilePath = path.join(__dirname, "logs", "access.log");
-
-// Create a write stream (in append mode) for the log file
-const accessLogStream = fs.createWriteStream(logFilePath, { flags: "a" });
-
-// Setup Winston logger
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: logFilePath }),
-  ],
-});
-
-let sessionConfig = {
-  name: "SESS_NAME",
-  secret: process?.env?.COOKIE_KEY || "secret",
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    sameSite: false,
-    secure: false,
-    httpOnly: true,
-  },
-  resave: false,
-  saveUninitialized: false,
-  store: MONGO_URL
-    ? MongoStore.create({
-        mongoUrl: MONGO_URL,
-      })
-    : "",
-};
-
-let corsOptions = {
-  credentials: true,
-  methods: [
-    "GET",
-    "HEAD",
-    "POST",
-    "PUT",
-    "DELETE",
-    "TRACE",
-    "OPTIONS",
-    "PATCH",
-  ],
-  origin: ["http://localhost:3001", "http://localhost:1234"],
-};
-
-let cspOptions = {
-  policies: {
-    "default-src": [
-      SELF,
-      // Add a allowed domains
-    ],
-    "connect-src": [SELF],
-  },
-};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -81,7 +28,12 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(morgan("common", { stream: accessLogStream }));
 app.use(morgan("common"));
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: !isDevelopment,
+    contentSecurityPolicy: !isDevelopment,
+  })
+);
 app.use(cors(corsOptions));
 app.use(expressCspHeader(cspOptions));
 
--- src/server.js
+++ src/server.js
@@ -6,12 +6,19 @@ const app = require("./app.js");
 
 const { mongoConnect } = require("@Services/mongo");
 
+const startApolloServer = require("@Services/graphql");
+const buildSchema = require("./scripts/graphqlBuildSchema.js");
+
 const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
 async function startServer() {
   await mongoConnect();
+
+  await buildSchema();
+
+  await startApolloServer();
 
   server.listen(PORT, () => {
     console.log(`App listning on PORT http://localhost:${PORT}`);
