b65b0cdceb1bfe87c67da051c2ac3682a944708f
--global <--global>
2024-06-28 23:54:34 +0530

    configurations is restructured from app

---
+++ src/config/helperVariables.js
@@ -0,0 +1,86 @@
+const MongoStore = require("connect-mongo");
+
+const path = require("path");
+const fs = require("fs");
+const winston = require("winston");
+
+const { MONGO_URL } = require("@Services/mongo");
+const { SELF } = require("express-csp-header");
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+let corsOptions = {
+  credentials: true,
+  methods: [
+    "GET",
+    "HEAD",
+    "POST",
+    "PUT",
+    "DELETE",
+    "TRACE",
+    "OPTIONS",
+    "PATCH",
+  ],
+  origin: ["http://localhost:3001", "http://localhost:1234"],
+};
+
+const config = {
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
+
+let sessionConfig = {
+  name: "SESS_NAME",
+  secret: process?.env?.COOKIE_KEY || "secret",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: false,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: MONGO_URL
+    ? MongoStore.create({
+        mongoUrl: MONGO_URL,
+      })
+    : "",
+};
+
+let cspOptions = {
+  policies: {
+    "default-src": [
+      SELF,
+      // Add a allowed domains
+    ],
+    "connect-src": [SELF],
+  },
+};
+
+// Define the log file path
+const logFilePath = path.join(__dirname, "..", "logs", "access.log");
+
+// Create a write stream (in append mode) for the log file
+const accessLogStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
+// Setup Winston logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: logFilePath }),
+  ],
+});
+
+module.exports = {
+  isDevelopment,
+  corsOptions,
+  cspOptions,
+  config,
+  sessionConfig,
+
+  logger,
+  accessLogStream,
+  logFilePath,
+};
